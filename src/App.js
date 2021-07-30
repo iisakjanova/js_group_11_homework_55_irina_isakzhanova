e488c8bc6de1acc3b1e0e00a432f352b207f802f
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-07-30 12:49:35 +0600

    Added drawing burger ingredients

--- src/App.js
+++ src/App.js
@@ -2,46 +2,46 @@ import {useState} from "react";
 import {nanoid} from "nanoid";
 
 import Ingredients from "./components/Ingredients/Ingredients";
-import './App.css'
 import Burger from "./components/Burger/Burger";
+import './App.css'
 
 const App = () => {
     const [ingredients, setIngredients] = useState([
-        {name: 'Meat', count: 0, id: nanoid(),},
-        {name: 'Cheese', count: 0, id: nanoid(),},
         {name: 'Salad', count: 0, id: nanoid(),},
+        {name: 'Cheese', count: 0, id: nanoid(),},
         {name: 'Bacon', count: 0, id: nanoid(),},
+        {name: 'Meat', count: 0, id: nanoid(),},
     ]);
 
     const increaseCount = id => {
-        setIngredients(prevIngredients => prevIngredients.map((ingredient) => {
+        setIngredients(prevIngredients => prevIngredients.map(ingredient => {
             if (ingredient.id === id) {
-                return {...ingredient, count: ingredient.count + 1}
+                return {...ingredient, count: ingredient.count + 1};
             }
 
             return ingredient;
-        }))
+        }));
     };
 
     const decreaseCount = id => {
-        setIngredients(prevIngredients => prevIngredients.map((ingredient) => {
+        setIngredients(prevIngredients => prevIngredients.map(ingredient => {
             if (ingredient.id === id) {
                 const count = ingredient.count !== 0 ? ingredient.count - 1 : 0;
-                return {...ingredient, count}
+                return {...ingredient, count};
             }
 
             return ingredient;
-        }))
+        }));
     };
 
     const removeIngredient = id => {
-        setIngredients(prevIngredients => prevIngredients.map((ingredient) => {
+        setIngredients(prevIngredients => prevIngredients.map(ingredient => {
             if (ingredient.id === id) {
-                return {...ingredient, count: 0}
+                return {...ingredient, count: 0};
             }
 
             return ingredient;
-        }))
+        }));
     };
 
     return (
@@ -52,7 +52,9 @@ const App = () => {
                 onDecrease={decreaseCount}
                 onRemove={removeIngredient}
             />
-            <Burger />
+            <Burger
+                ingredients={ingredients}
+            />
         </div>
     );
 };
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,3 +1,5 @@
+import {nanoid} from "nanoid";
+
 import React from 'react';
 import './Burger.css';
 import BreadTop from "../BreadTop/BreadTop";
@@ -7,17 +9,51 @@ import Cheese from "../Cheese/Cheese";
 import Meat from "../Meat/Meat";
 import Bacon from "../Bacon/Bacon";
 
-const Burger = () => {
+const Burger = props => {
+    const getIngredientComponent = (name) => {
+        let ingredient;
+
+        switch (name) {
+            case 'Meat':
+                ingredient = <Meat key={nanoid()} />;
+                break;
+            case 'Cheese':
+                ingredient = <Cheese key={nanoid()} />;
+                break;
+            case 'Bacon':
+                ingredient = <Bacon key={nanoid()} />;
+                break;
+            case 'Salad':
+                ingredient = <Salad key={nanoid()} />;
+                break;
+            default:
+                ingredient = <Salad key={nanoid()} />;
+                break;
+        }
+
+        return ingredient;
+    };
+
+    const prepareIngredients = () => {
+        const stuffing = [];
+
+        props.ingredients.forEach(({count, name}) => {
+            for (let i = 0; i < count; i++) {
+                const component = getIngredientComponent(name);
+                stuffing.push(component);
+            }
+        });
+
+        return stuffing;
+    };
+
     return (
         <div className="BurgerWrapper">
             <fieldset>
                 <legend>Burger</legend>
                 <div className="Burger">
                     <BreadTop />
-                    <Salad />
-                    <Cheese />
-                    <Meat />
-                    <Bacon />
+                    {prepareIngredients()}
                     <BreadBottom />
                 </div>
             </fieldset>
